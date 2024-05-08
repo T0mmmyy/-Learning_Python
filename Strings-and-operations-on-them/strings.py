a729db10158a7dfa25cd8360c3352c35fd586e49
T0mmmyy <tommilevis6@gmail.com>
2024-05-09 01:53:37 +0500

    strings

---
+++ Strings-and-operations-on-them/strings.py
@@ -0,0 +1,13 @@
+s = 'hello'
+print(len(s))
+b = input('Введите любое кол-во символов ')
+print('Вы ввели', len(b),  'символов')
+a = 'Hello'
+b = "l" in a
+print(b)
+aa = 'aaa'
+g = aa == 'aaa'
+print(g)
+f = aa == 'AAA'
+print(f)
+print(ord('a'))
